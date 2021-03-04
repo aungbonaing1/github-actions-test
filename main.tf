0738cc54a922cf0e6f520c17e6a3e79ddde00ec4
aungbonaing1 <aungbo.naing@gmail.com>
2021-03-04 10:35:05 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "allow_tls" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.main.cidr_block]
+    cidr_blocks = ["192.168.1.1/16"]
   }
 
   egress {
